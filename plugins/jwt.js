b9227de27f3fac38839dbf7720c1439efa01c07a
epitexam <202238868+epitexam@users.noreply.github.com>
2025-03-08 23:06:50 +0100

    Implementing new hooks to verify user roles

--- plugins/jwt.js
+++ plugins/jwt.js
@@ -29,4 +29,56 @@ module.exports = fp(async function (fastify, opts) {
             reply.send(err)
         }
     })
+
+    fastify.decorate("isAdmin", async function (request, reply) {
+        try {
+            const { user } = request
+
+            const userInfo = await fastify.prisma.user.findUnique({
+                where: {
+                    id: user.id
+                },
+                select: {
+                    role: true
+                }
+            })
+
+            if (!userInfo) {
+                return reply.status(401).send({ message: "This user does not exist." })
+            }
+
+            if (userInfo.role < 3) {
+                return reply.status(401).send({ message: "You are not allowed to perform this action." })
+            }
+
+        } catch (err) {
+            reply.send(err)
+        }
+    })
+
+    fastify.decorate("isProfessor", async function (request, reply) {
+        try {
+            const { user } = request
+
+            const userInfo = await fastify.prisma.user.findUnique({
+                where: {
+                    id: user.id
+                },
+                select: {
+                    role: true
+                }
+            })
+
+            if (!userInfo) {
+                return reply.status(401).send({ message: "This user does not exist." })
+            }
+
+            if (userInfo.role < 2) {
+                return reply.status(401).send({ message: "You are not allowed to perform this action." })
+            }
+
+        } catch (err) {
+            reply.send(err)
+        }
+    })
 })
